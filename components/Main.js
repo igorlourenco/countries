7e14f8d95472e1f4da4042597c638499390aa779
Igor Lourenço <43149318+igorlrnc@users.noreply.github.com>
2020-09-19 08:50:15 -0300

    Initial commit

---
+++ components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "@chakra-ui/core";
+
+export default function Main() {
+    return (
+        <Text> Esse é o template inicial para aplicações Next.js </Text>
+    )
+}
